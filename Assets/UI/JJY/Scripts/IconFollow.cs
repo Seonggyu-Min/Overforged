2d72edd1e21f286e46ec9cb1f76f85d1b25e52c7
정재영 <wodud04277@gmail.com>
2025-07-24 09:46:51 +0900

    [chore] main cam rot, IconFollow
    카메라 각도 수정 및 아이콘 위치 재설정

--- Assets/UI/JJY/Scripts/IconFollow.cs
+++ Assets/UI/JJY/Scripts/IconFollow.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class IconFollow : MonoBehaviour
 {
     public Transform target; // 머리위 빈 오브젝트
-    public Vector3 offset = Vector3.up * 3f;
 
     void LateUpdate()
     {
         if (target == null) return;
 
         Debug.Log("바라보기");
-        transform.position = target.position + offset;
-        // transform.forward = Camera.main.transform.forward;
+        transform.position = target.position;
+        transform.forward = Camera.main.transform.forward;
     }
 }
