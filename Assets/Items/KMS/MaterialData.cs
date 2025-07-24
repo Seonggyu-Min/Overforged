171070aa0492c104ce015fb110112e4790885c64
munsung143 <akunta3475@gmail.com>
2025-07-24 14:55:10 +0900

    [Feat] 아이템 클래스 구현중

---
+++ Assets/Items/KMS/MaterialData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialData : ScriptableObject
+{
+
+    public Material Gold;
+    public Material Copper;
+    public Material Steel;
+    public Material Wood;
+
+    public Material Stone;
+    public Material None;
+
+
+}
