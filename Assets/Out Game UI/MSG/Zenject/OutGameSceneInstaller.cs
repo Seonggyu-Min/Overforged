7a79ad1b7179abdf54a39a741b839c7d2aedf4ea
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-24 17:23:58 +0900

    feat: 아웃게임 씬용 바인딩 설정 클래스 추가
    - OutGameUIManager, NetworkManager 등록

---
+++ Assets/Out Game UI/MSG/Zenject/OutGameSceneInstaller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+
+namespace MIN
+{
+    /// <summary>
+    /// 인게임 씬이 아닌 아웃게임 씬에 종속된 MonoInstaller
+    /// </summary>
+    public class OutGameSceneInstaller : MonoInstaller
+    {
+        [SerializeField] private GameObject _outGameUIManagerPrefab;
+        [SerializeField] private GameObject _networkManagerPrefab;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<IOutGameUIManager>()
+                .To<OutGameUIManager>()
+                .FromComponentInNewPrefab(_outGameUIManagerPrefab)
+                .AsSingle()
+                .NonLazy();
+
+            Container.Bind<INetworkManager>()
+                .To<NetworkManager>()
+                .FromComponentInNewPrefab(_networkManagerPrefab)
+                .AsSingle()
+                .NonLazy();
+        }
+    }
+}
