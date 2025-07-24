607afcb4ffed64c462d32740769e373c9be72022
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-24 12:09:18 +0900

    feat: 아이템 SO 구현
    - 해당 SO 데이터의 필드에 다음으로 변할 아이템 SO를 등록하여 바로 접근할 수 있는 구조로 구현

---
+++ Assets/Items/MSG/ItemData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MIN
+{
+    [CreateAssetMenu(menuName = "ItemSO")]
+    public class ItemData : ScriptableObject
+    {
+        public int ID;
+        public string Name;
+
+        public ItemType ItemType;
+        public OreType OreType;
+        public WoodType WoodType;
+
+        public bool NeedTongs;        // 집게가 필요한지 여부
+        public Sprite Image;
+        public GameObject ItemPrefab; // 현재 아이템 프리팹
+        public ItemData NextItem;     // 다음 아이템 SO
+
+        // public float TimeNeeded;
+
+        //public InteractionObject InterationObj; // 모루 등
+
+        // public ItemData FinishedItem;
+        public ItemType CombineObjType;
+    }
+}
