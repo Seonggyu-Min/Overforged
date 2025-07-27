ff16c0f770e9d91bb0046e6ce68251576940e719
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-28 02:15:18 +0900

    feat: 캐릭터 스프라이트 등록용 클래스 추가

---
+++ Assets/Out Game UI/MSG/Utils/CharacterSpriteRegistrar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MIN
+{
+    public class CharacterSpriteRegistrar : MonoBehaviour
+    {
+        [SerializeField] private Sprite[] characterSprites;
+
+        private void Start()
+        {
+            CustomPropertyDatabase.RegisterCharacterSprite(CharacterId.Bunny, characterSprites[0]);
+            CustomPropertyDatabase.RegisterCharacterSprite(CharacterId.Cat, characterSprites[1]);
+            CustomPropertyDatabase.RegisterCharacterSprite(CharacterId.Dog, characterSprites[2]);
+            CustomPropertyDatabase.RegisterCharacterSprite(CharacterId.Duck, characterSprites[3]);
+        }
+    }
+}
