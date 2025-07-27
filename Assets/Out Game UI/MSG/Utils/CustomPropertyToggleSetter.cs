bdc64bbc57e36220c0617ee4dd631b06d4bd5075
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-28 02:16:25 +0900

    feat: 캐릭터 및 팀 토글의 커스텀 프로퍼티 업데이트 로직 추가

---
+++ Assets/Out Game UI/MSG/Utils/CustomPropertyToggleSetter.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace MIN
+{
+    public class CustomPropertyToggleSetter : MonoBehaviour
+    {
+        [Header("커스텀 프로퍼티 키")]
+        public string propertyKey;
+
+        [Header("적용할 int 값")]
+        public int propertyValue;
+
+        private Toggle _toggle;
+
+        private void Awake()
+        {
+            _toggle = GetComponent<Toggle>();
+            _toggle.onValueChanged.AddListener(OnToggleChanged);
+        }
+
+        private void OnToggleChanged(bool isOn)
+        {
+            if (!isOn) return;
+
+            ExitGames.Client.Photon.Hashtable props = new ExitGames.Client.Photon.Hashtable
+            {
+                { propertyKey, propertyValue }
+            };
+            PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+        }
+    }
+}
