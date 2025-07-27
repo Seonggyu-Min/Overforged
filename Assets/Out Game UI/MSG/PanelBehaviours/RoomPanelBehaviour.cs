1704d11f60310cfa984378f81bdeca53bc6bb62f
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-28 02:12:59 +0900

    feat: 방 이름 표기 기능 추가

--- Assets/Out Game UI/MSG/PanelBehaviours/RoomPanelBehaviour.cs
+++ Assets/Out Game UI/MSG/PanelBehaviours/RoomPanelBehaviour.cs
@@ -1,12 +1,36 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 
 namespace MIN
 {
-    public class RoomPanelBehaviour : MonoBehaviour
+    public class RoomPanelBehaviour : MonoBehaviourPunCallbacks
     {
+        [SerializeField] private TMP_Text _roomNameText;
 
+
+        public override void OnEnable()
+        {
+            StartCoroutine(RoomNameUpdateRoutine());
+        }
+
+        private IEnumerator RoomNameUpdateRoutine()
+        {
+            float timeout = 3f;
+            float timer = 0f;
+
+            while (PhotonNetwork.CurrentRoom == null && timer < timeout)
+            {
+                timer += 0.5f;
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            _roomNameText.text = PhotonNetwork.CurrentRoom != null
+                ? PhotonNetwork.CurrentRoom.Name
+                : "방 없음";
+        }
     }
 }
