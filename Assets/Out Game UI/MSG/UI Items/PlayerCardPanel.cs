f5903ec1766fbef5b03f556c3fafc2605e5406ac
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-28 02:14:18 +0900

    feat: 플레이어 카드 업데이트 기능 추가

--- Assets/Out Game UI/MSG/UI Items/PlayerCardPanel.cs
+++ Assets/Out Game UI/MSG/UI Items/PlayerCardPanel.cs
@@ -12,7 +12,10 @@ namespace MIN
 {
     public class PlayerCardPanel : MonoBehaviourPunCallbacks
     {
+        #region Fields And Properties
+
         [Inject] private DiContainer _diContainer;
+        [Inject] private IOutGameUIManager _outGameUIManager;
 
         [SerializeField] private GameObject _playerCardPrefab;
         [SerializeField] private Transform _playerContentParent;
@@ -22,18 +25,68 @@ namespace MIN
         private bool _isReady = false;
 
         [SerializeField] private TMP_Text _infoText;
+        [SerializeField] private TMP_Text _debugText;
+
+        private Dictionary<int, PlayerCardItem> _playerCardDict = new();
+
+        #endregion
 
 
-        public override void OnEnable()
+        #region Unity Methods
+
+        public override void OnEnable() => PhotonNetwork.AddCallbackTarget(this);
+        
+        public override void OnDisable() => PhotonNetwork.RemoveCallbackTarget(this);
+
+        // 디버그용
+        private void Update()
         {
-            InitButtonText();
+            _debugText.text = $"Player Count: {PhotonNetwork.PlayerList.Length}\n" +
+                              $"Is Master Client: {PhotonNetwork.IsMasterClient}\n" +
+                              $"Is In Room: {PhotonNetwork.InRoom}\n" +
+                              $"Is Connected: {PhotonNetwork.IsConnected}\n" +
+                              $"PlayerActorNum: {PhotonNetwork.LocalPlayer.ActorNumber}";
+        }
+
+        #endregion
+
+
+        #region Photon Callbacks
+
+        public override void OnJoinedRoom()
+        {
             UpdatePlayerCards();
+            UpdateButtonText();
         }
 
         public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
         {
+            if (_playerCardDict.TryGetValue(targetPlayer.ActorNumber, out PlayerCardItem card))
+            {
+                card.UpdateStatus(targetPlayer);
+            }
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
             UpdatePlayerCards();
         }
+
+        public override void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            UpdatePlayerCards();
+        }
+
+        public override void OnMasterClientSwitched(Player newMasterClient)
+        {
+            UpdatePlayerCards();
+            UpdateButtonText();
+        }
+
+        #endregion
+
+
+        #region Public Methods
 
         public void OnClickReadyOrStartButton()
         {
@@ -45,9 +98,21 @@ namespace MIN
                     _infoText.text = "최소 2명 이상이 필요합니다.";
                     return;
                 }
-                PhotonNetwork.CurrentRoom.IsOpen = false; // 방을 닫음
-                PhotonNetwork.CurrentRoom.IsVisible = false; // 방을 비공개로 설정
-                PhotonNetwork.LoadLevel("GameScene"); // 게임 씬 로드
+
+                // 모든 플레이어가 준비 상태인지 확인
+                foreach (var player in PhotonNetwork.PlayerList)
+                {
+                    if (player.IsMasterClient) continue; // 마스터 클라이언트는 제외
+                    if (!player.CustomProperties.TryGetValue(CustomPropertyKeys.IsReady, out object isReadyObj) || !(bool)isReadyObj)
+                    {
+                        _infoText.text = "모든 플레이어가 준비 상태여야 합니다.";
+                        return;
+                    }
+                }
+
+                PhotonNetwork.CurrentRoom.IsOpen = false;
+                PhotonNetwork.CurrentRoom.IsVisible = false;
+                PhotonNetwork.LoadLevel("GameScene");
             }
             else
             {
@@ -55,29 +120,81 @@ namespace MIN
                 _isReady = !_isReady;
                 PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { CustomPropertyKeys.IsReady, _isReady } });
             }
+
+            UpdateButtonText();
         }
 
-        private void InitButtonText()
+        public void OnClickExitButton()
         {
-            _readyButtonText.text = PhotonNetwork.IsMasterClient ? "Start" : "Ready";
+            _outGameUIManager.CloseTopPanel();
         }
+
+        #endregion
+
+
+        #region Private Methods
+
+        private void UpdateButtonText()
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                _readyButtonText.text = "Start";
+            }
+            else
+            {
+                if (_isReady)
+                {
+                    _readyButtonText.text = "Unready";
+                }
+                else
+                {
+                    _readyButtonText.text = "Ready";
+                }
+            }
+        }
+
 
         private void UpdatePlayerCards()
         {
-            _readyButtonText.text = _isReady ? "Unready" : "Ready";
+            // 현재 방에 존재하는 플레이어 ActorNumber 저장
+            HashSet<int> currentActors = new();
 
-            // PlayerCard 모두 제거
-            foreach (Transform child in _playerContentParent)
+            foreach (Player player in PhotonNetwork.PlayerList)
             {
-                Destroy(child.gameObject);
+                currentActors.Add(player.ActorNumber);
+
+                if (_playerCardDict.TryGetValue(player.ActorNumber, out PlayerCardItem existingCard))
+                {
+                    // 이미 존재하는 카드가 있으면 업데이트
+                    existingCard.UpdateStatus(player);
+                }
+                else
+                {
+                    // 새로운 플레이어 카드 생성
+                    PlayerCardItem newCard = _diContainer.InstantiatePrefabForComponent<PlayerCardItem>(_playerCardPrefab, _playerContentParent);
+                    newCard.Init(player);
+                    _playerCardDict[player.ActorNumber] = newCard;
+                }
             }
 
-            // 현재 플레이어의 카드 생성
-            foreach (var player in PhotonNetwork.PlayerList)
+            // 방에서 나간 플레이어 카드 제거
+            List<int> toRemove = new();
+            foreach (var kvp in _playerCardDict)
             {
-                PlayerCardItem playerCardItem = _diContainer.InstantiatePrefabForComponent<PlayerCardItem>(_playerCardPrefab, _playerContentParent);
-                playerCardItem.Init(player);
+                if (!currentActors.Contains(kvp.Key))
+                {
+                    Destroy(kvp.Value.gameObject);
+                    toRemove.Add(kvp.Key);
+                }
+            }
+
+            // 제거할 플레이어 카드 딕셔너리에서 제거
+            foreach (int actorNumber in toRemove)
+            {
+                _playerCardDict.Remove(actorNumber);
             }
         }
+
+        #endregion
     }
 }
