f3466378bc3231f6fe001fdf968ad6f2c10fd0b5
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-28 17:06:27 +0900

    feat: 게임매니저 로직 추가
    - 승패 계산 및 데이터 저장 로직 구현
    - 아웃게임 씬으로 이동 로직 구현

---
+++ Assets/Out Game UI/MSG/Managers/IGameManager.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MIN
+{
+    public interface IGameManager
+    {
+        public void GoToRoom();
+        public void CalculateResult(List<MatchPlayerData> result);
+    }
+}
