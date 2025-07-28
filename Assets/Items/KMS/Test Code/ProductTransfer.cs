05e1f0b8e013b84b7720e791099648e545d9fc51
munsung143 <akunta3475@gmail.com>
2025-07-28 17:23:31 +0900

    아이템 출품 구현 중

---
+++ Assets/Items/KMS/Test Code/ProductTransfer.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using SHG;
+using UnityEngine;
+
+public class ProductTransfer : MonoBehaviour, IInteractableTool
+{
+    [SerializeField] ItemDataList itemdata;
+
+    [SerializeField] MaterialData materialData;
+
+    private ProductType prod;
+    private OreType ore;
+    private WoodType wood;
+
+    [SerializeField] GameObject productItem;
+
+    [SerializeField] Canvas UI;
+
+    private List<ProductType> datas;
+    private List<OreType> ores;
+    private List<WoodType> woods;
+
+
+    void Awake()
+    {
+        UI.enabled = false;
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            if (Enum.IsDefined(typeof(ProductType), i))
+            {
+                datas[i] = (ProductType)i;
+            }
+            else break;
+        }
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            if (Enum.IsDefined(typeof(OreType), i))
+            {
+                ores[i] = (OreType)i;
+            }
+            else break;
+        }
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            if (Enum.IsDefined(typeof(WoodType), i))
+            {
+                woods[i] = (WoodType)i;
+            }
+            else break;
+        }
+
+    }
+
+
+    public bool CanTransferItem(ToolTransferArgs args)
+    {
+        return false;
+    }
+
+    public ToolTransferResult Transfer(ToolTransferArgs args)
+    {
+        UI.enabled = false;
+        return (new ToolTransferResult
+        {
+            ReceivedItem = null,
+            IsDone = true
+        });
+    }
+
+    public bool CanWork()
+    {
+        return true;
+    }
+
+
+    //상호작용 시 UI가 켜져있으면 끄고, 꺼져있으면 키고 못움직이도록 해보았습니다.
+
+    public ToolWorkResult Work()
+    {
+        if (UI.enabled)
+        {
+            UI.enabled = false;
+            return (new ToolWorkResult
+            {
+                Trigger = null,
+                DurationToStay = 0.1f
+            });
+
+        }
+        else
+        {
+            UI.enabled = true;
+            return (new ToolWorkResult
+            {
+                Trigger = null,
+                DurationToStay = float.MaxValue
+            });
+        }
+    }
+
+    public void GenRequest()
+    {
+        int d = UnityEngine.Random.Range(0, datas.Count);
+        int o = UnityEngine.Random.Range(1, ores.Count);
+        int w = UnityEngine.Random.Range(1, woods.Count);
+
+        prod = datas[d];
+        wood = woods[w];
+        if (prod != ProductType.Bow)
+        {
+            ore = ores[o];
+        }
+    }
+
+    public void GenItem()
+    {
+        GameObject go = Instantiate(productItem);
+        ProductItem HoldingItem = go.GetComponent<ProductItem>();
+
+        if (prod == ProductType.Bow)
+        {
+            
+        }
+        if (ore != OreType.None) HoldingItem.Ore = ore;
+        if (wood != WoodType.None) HoldingItem.Wood = wood;
+
+        TestPlayerControl player = GameObject.Find("Player").GetComponent<TestPlayerControl>();
+        Transform hand = player.hand;
+        player.current = HoldingItem;
+        HoldingItem.Go(hand);
+        HoldingItem = null;
+        UI.enabled = false;
+    }
+
+}
