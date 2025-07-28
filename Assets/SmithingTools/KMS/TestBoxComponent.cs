45340015141a2b9ec14fa78d4b513286f881bba1
munsung143 <akunta3475@gmail.com>
2025-07-28 11:13:37 +0900

    [feat] 아이템 박스

--- Assets/SmithingTools/KMS/TestBoxComponent.cs
+++ Assets/SmithingTools/KMS/TestBoxComponent.cs
@@ -17,23 +17,23 @@ public class TestBoxComponent : MonoBehaviour, IInteractableTool
 
     [SerializeField] GameObject matItem;
 
+    [SerializeField] Canvas UI;
+
 
     void Awake()
     {
-
+        UI.enabled = false;
     }
 
 
     public bool CanTransferItem(ToolTransferArgs args)
     {
-        //손에 든 것이 없을때만 가능하도록
-        return (args.ItemToGive == null);
-
+        return false;
     }
 
     public ToolTransferResult Transfer(ToolTransferArgs args)
     {
-        GenerateItem();
+        UI.enabled = false;
         var item = HoldingItem;
         this.HoldingItem = null;
         return (new ToolTransferResult
@@ -45,22 +45,67 @@ public class TestBoxComponent : MonoBehaviour, IInteractableTool
 
     public bool CanWork()
     {
-        return false;
-
+        return true;
     }
+
+
+    //상호작용 시 UI가 켜져있으면 끄고, 꺼져있으면 키고 못움직이도록 해보았습니다.
 
     public ToolWorkResult Work()
     {
-        return new ToolWorkResult();
+        if (UI.enabled)
+        {
+            UI.enabled = false;
+            return (new ToolWorkResult
+            {
+                Trigger = null,
+                DurationToStay = 0.1f
+            });
+
+        }
+        else
+        {
+            UI.enabled = true;
+            return (new ToolWorkResult
+            {
+                Trigger = null,
+                DurationToStay = float.MaxValue
+            });
+        }
     }
 
-    private void GenerateItem()
+    private void GenerateItem(int id, OreType ore, WoodType wood)
     {
         GameObject go = Instantiate(matItem);
-        go.transform.position = transform.position;
         HoldingItem = go.GetComponent<MaterialItem>();
-        HoldingItem.Data = itemdata.list[3];
-        HoldingItem.Ore = OreType.Gold;
-        Debug.Log(HoldingItem.Name);
+        HoldingItem.Data = itemdata.list[id];
+        if (ore != OreType.None) HoldingItem.Ore = ore;
+        if (wood != WoodType.None) HoldingItem.Wood = wood;
+        Transfer(new ToolTransferArgs());
+    }
+
+    public void GenIronOre()
+    {
+        GenerateItem(3, OreType.Steel, WoodType.None);
+    }
+    public void GenCopperOre()
+    {
+        GenerateItem(3, OreType.Copper, WoodType.None);
+    }
+    public void GenGoldOre()
+    {
+        GenerateItem(3, OreType.Gold, WoodType.None);
+    }
+    public void GenOakWood()
+    {
+        GenerateItem(0, OreType.None, WoodType.Oak);
+    }
+    public void GenBirchWood()
+    {
+        GenerateItem(0, OreType.None, WoodType.Birch);
+    }
+    public void GenString()
+    {
+        GenerateItem(2, OreType.None, WoodType.None);
     }
 }
