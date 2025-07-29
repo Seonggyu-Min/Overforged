ed5ba948b699e2f4743401f57adea05636f8ba0b
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-29 13:10:15 +0900

    feat: 점수 관리용 매니저 클래스 추가
    - SceneContext에 바인드 되어 인게임 씬 한정으로 주입받아 사용
    - 플레이어 커스텀 프로퍼티에서 점수 관리

---
+++ Assets/Out Game UI/MSG/Managers/IScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MIN
+{
+    public interface IScoreManager
+    {
+        public void AddScore(Player player ,int score);
+
+    }
+}
