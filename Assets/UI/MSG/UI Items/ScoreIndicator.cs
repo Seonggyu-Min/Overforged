0e358fb9b6356bf28cd3eda8121962bc1ace6dc7
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-29 13:07:18 +0900

    feat: 점수 표시 로직 추가

---
+++ Assets/UI/MSG/UI Items/ScoreIndicator.cs
@@ -0,0 +1,38 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEditor.Hardware;
+using UnityEngine;
+
+namespace MIN
+{
+    public class ScoreIndicator : MonoBehaviourPunCallbacks
+    {
+        [SerializeField] private TMP_Text _scoreText;
+
+        private void Awake()
+        {
+            if (_scoreText == null)
+            {
+                _scoreText = GetComponent<TMP_Text>();
+            }
+        }
+
+        public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+        {
+            if (targetPlayer != PhotonNetwork.LocalPlayer) return;
+
+            if (targetPlayer.CustomProperties.TryGetValue(CustomPropertyKeys.Score, out var score))
+            {
+                _scoreText.text = $"{targetPlayer.NickName}: {score}";
+            }
+            else
+            {
+                _scoreText.text = $"{targetPlayer.NickName}: 0";
+            }
+        }
+    }
+}
