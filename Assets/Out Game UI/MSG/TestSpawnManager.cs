f8dfab46adb47b4a5cfd3fc85cc2c71ff236f75a
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-29 10:21:35 +0900

    feat: 테스트용 스폰매니저 생성

---
+++ Assets/Out Game UI/MSG/TestSpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SCR;
+
+namespace MIN
+{
+    public class TestSpawnManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            SpwanPlayer();
+        }
+
+        [ContextMenu("SpawnPlayer")]
+        public void SpwanPlayer()
+        {
+            Vector3 spawnPos = new Vector3(0, 0.5f, 0);
+            GameObject playerobj = PhotonNetwork.Instantiate("Player", spawnPos, Quaternion.identity);
+            Player player = playerobj.GetComponent<Player>();
+        }
+    }
+}
+
