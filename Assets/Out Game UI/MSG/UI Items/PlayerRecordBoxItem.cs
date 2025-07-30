e4537c3c02db0ea2a1853ec232ffe8d7ff3e9c91
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-30 14:23:47 +0900

    feat: 승률 UI 로직 추가

---
+++ Assets/Out Game UI/MSG/UI Items/PlayerRecordBoxItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+namespace MIN
+{
+    public class PlayerRecordBoxItem : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _nickNameText;
+        [SerializeField] private TMP_Text _winRateText;
+        [SerializeField] private TMP_Text _winLoseDrawCountText;
+
+        public void SetPlayerRecord(string nickName, float winRate, int winCount, int loseCount, int drawCount)
+        {
+            _nickNameText.text = nickName;
+            _winRateText.text = $"{winRate * 100:F2}%";
+            _winLoseDrawCountText.text = $"{winCount}승 {loseCount}패 {drawCount}무";
+        }
+    }
+}
