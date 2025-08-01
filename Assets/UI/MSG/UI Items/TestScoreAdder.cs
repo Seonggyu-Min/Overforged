859c4c1fb78d8b363be1f96a46ef2e20d985762e
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 11:41:33 +0900

    feat: 테스트용 승패 계산 시작 기능 추가

--- Assets/UI/MSG/UI Items/TestScoreAdder.cs
+++ Assets/UI/MSG/UI Items/TestScoreAdder.cs
@@ -29,6 +29,42 @@ namespace MIN
                 Debug.Log("게임 종료는 마스터 클라이언트만 수행할 수 있습니다.");
             }
         }
+
+        public void OnClickCalculateWinLoseButton()
+        {
+            CalculatedPlayer calculatedPlayer = _gameManager.CalculateResult();
+
+            foreach (var p in calculatedPlayer.WinPlayers)
+            {
+                if (p == PhotonNetwork.LocalPlayer)
+                {
+                    // 승리 UI
+                }
+
+                Debug.Log($"승리한 플레이어: {p.NickName}");
+            }
+
+            foreach (var p in calculatedPlayer.LosePlayers)
+            {
+                if (p == PhotonNetwork.LocalPlayer)
+                {
+                    // 패배 UI
+                }
+
+                Debug.Log($"패배한 플레이어: {p.NickName}");
+            }
+
+            foreach (var p in calculatedPlayer.DrawPlayers)
+            {
+                if (p == PhotonNetwork.LocalPlayer)
+                {
+                    // 무승부 UI
+                }
+
+                Debug.Log($"무승부 플레이어: {p.NickName}");
+            }
+            Debug.Log("승패 계산 요청을 보냈습니다.");
+        }
     }
 }
 
