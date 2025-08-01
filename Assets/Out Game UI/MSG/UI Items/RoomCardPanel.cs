3efafa3e59666e7406b6740cafce1ecabd9df7eb
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-01 17:27:43 +0900

    refactor: 페이지 수 표기 삭제를 위한 주석처리

--- Assets/Out Game UI/MSG/UI Items/RoomCardPanel.cs
+++ Assets/Out Game UI/MSG/UI Items/RoomCardPanel.cs
@@ -21,7 +21,7 @@ namespace MIN
 
         [SerializeField] private Button _leftArrowButton;
         [SerializeField] private Button _rightArrowButton;
-        [SerializeField] private TMP_Text _roomPageText;
+        //[SerializeField] private TMP_Text _roomPageText;
 
         private List<RoomInfo> _roomList = new();
         private int _currentPage = 0;
@@ -62,7 +62,7 @@ namespace MIN
 
             // 페이지 텍스트 업데이트
             int totalPages = Mathf.Max(1, Mathf.CeilToInt((float)_roomList.Count / RoomsPerPage)); // 방이 0개일 때도 1페이지는 존재하는 것 처럼 표기
-            _roomPageText.text = $"Page {_currentPage + 1} / {totalPages}";
+            //_roomPageText.text = $"Page {_currentPage + 1} / {totalPages}";
         }
 
 
