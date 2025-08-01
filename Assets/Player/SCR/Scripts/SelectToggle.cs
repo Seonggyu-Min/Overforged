9caf3adc82a807edd4755924f96d9bf4033a5331
FeAnd <tcf1268@gmail.com>
2025-08-01 18:01:19 +0900

    Update UI 에셋 적용

--- Assets/Player/SCR/Scripts/SelectToggle.cs
+++ Assets/Player/SCR/Scripts/SelectToggle.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +6,9 @@ namespace SCR
 {
     public class SelectToggle : MonoBehaviour
     {
+        [Header("커스텀 프로퍼티 키")]
+        public string propertyKey;
+
         [SerializeField] Color selectColor;
         [SerializeField] Color unSelectColor;
         [SerializeField] Toggle[] toggle;
@@ -33,6 +35,13 @@ namespace SCR
 
                 toggle[i].colors = colorBlock;
             }
+            ExitGames.Client.Photon.Hashtable props = new ExitGames.Client.Photon.Hashtable
+            {
+                { propertyKey, selectIndex }
+            };
+            PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+
+
         }
     }
 }
