543257588abedc2667eb4456b21cf74dd87a46e8
FeAnd <tcf1268@gmail.com>
2025-08-03 00:07:06 +0900

    Feat 결과창 UI 추가
    게임이 끝나고 n초 동안 띄워지는 형식의 결과 창을 추가하였다.

--- Assets/UI/SCR/ScoreStatusUser.cs
+++ Assets/UI/SCR/ScoreStatusUser.cs
@@ -6,20 +6,30 @@ namespace SCR
 {
     public class ScoreStatusUser : MonoBehaviour
     {
+        [SerializeField] TeamColor color;
+        [SerializeField] Image teamColor;
         [SerializeField] Image playerImage;
         [SerializeField] TMP_Text playerNickname;
         [SerializeField] TMP_Text playerScore;
 
-        public void SetPlayer(Sprite profile, string Nickname)
+        public void SetPlayer(Sprite profile, string Nickname, int team)
         {
             playerImage.sprite = profile;
             playerNickname.text = Nickname;
+            teamColor.color = color.Color[team];
             SetScore(0);
         }
 
         public void SetScore(int Score)
         {
             playerScore.text = $"{Score}";
+        }
+
+        public void SetResult(string Nickname, int team, int Score)
+        {
+            playerNickname.text = Nickname;
+            teamColor.color = color.Color[team];
+            SetScore(Score);
         }
     }
 }
