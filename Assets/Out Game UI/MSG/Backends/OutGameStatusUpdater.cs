cc20cf4891eb28fb3da60f7f9904b54fb44c8483
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-04 14:12:40 +0900

    feat: 온라인 상태 업데이트 로직 추가
    - 코루틴을 통해 일정 시간 마다 자신의 uid에 시간을 업데이트하는 로직 추가

---
+++ Assets/Out Game UI/MSG/Backends/OutGameStatusUpdater.cs
@@ -0,0 +1,51 @@
+﻿using Firebase.Database;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+
+namespace MIN
+{
+    public class OutGameStatusUpdater : MonoBehaviour
+    {
+        [Inject] private IFirebaseManager _firebaseManager;
+
+        [SerializeField] private float _sendInterval = 10f;
+        private Coroutine _SendOnlineCO;
+
+        private void OnEnable()
+        {
+            _SendOnlineCO = StartCoroutine(SendOnlineStatusRoutine());
+        }
+
+        private void OnDisable()
+        {
+            if (_SendOnlineCO != null)
+                StopCoroutine(_SendOnlineCO);
+        }
+
+        private IEnumerator SendOnlineStatusRoutine()
+        {
+            while (true)
+            {
+                UpdateLastSeen();
+                yield return new WaitForSeconds(_sendInterval);
+            }
+        }
+
+        private void UpdateLastSeen()
+        {
+            string uid = _firebaseManager.Auth.CurrentUser.UserId;
+            DatabaseReference statusRef = _firebaseManager.Database.GetReference("lobbyStatus").Child(uid);
+
+            Dictionary<string, object> updateData = new()
+            {
+                ["lastSeen"] = ServerValue.Timestamp,
+                ["isGaming"] = false
+            };
+
+            statusRef.UpdateChildrenAsync(updateData);
+        }
+    }
+}
