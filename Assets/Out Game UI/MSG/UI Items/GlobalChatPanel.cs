0cc0cab835c432044167ab6c55b4b3c035516216
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-05 17:19:40 +0900

    feat: 전체 채팅 패널 로직 추가

--- Assets/Out Game UI/MSG/UI Items/GlobalChatPanel.cs
+++ Assets/Out Game UI/MSG/UI Items/GlobalChatPanel.cs
@@ -1,62 +1,140 @@
-﻿using ExitGames.Client.Photon;
-using Photon.Pun;
-using Photon.Realtime;
+﻿using Firebase.Database;
+using Firebase.Extensions;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
 
 namespace MIN
 {
-    // TODO: ChatBoxItem 풀링
-    //public class GlobalChatPanel : MonoBehaviour, IOnEventCallback
-    //{
-    //    [SerializeField] private TMP_InputField _messageInputField;
-    //    [SerializeField] private Transform _chatContentParent;
-    //    [SerializeField] private GameObject _chatBoxPrefab;
+    [Serializable]
+    public class ChatMessage
+    {
+        public string sender;
+        public string text;
+        public long timestamp;
 
-    //    private void OnEnable() => PhotonNetwork.AddCallbackTarget(this);
+        public ChatMessage(string sender, string text)
+        {
+            this.sender = sender;
+            this.text = text;
+            this.timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+    }
 
-    //    private void OnDisable() => PhotonNetwork.RemoveCallbackTarget(this);
+    public class GlobalChatPanel : MonoBehaviour
+    {
+        [Inject] private IFirebaseManager _firebaseManager;
+
+        [SerializeField] private List<ChatBoxItem> chatBoxItems;
+        [SerializeField] private ScrollRect _scrollRect;
+        [SerializeField] private TMP_InputField _textInputField;
+
+        private Queue<ChatBoxItem> chatItems = new(); // 재사용 큐
+        private List<ChatBoxItem> activeItems = new(); // 순서 추적용 리스트
+        private const int MaxChatCount = 30;
+        private Query _chatRef;
+
+        private void Start()
+        {
+            // 초기 채팅 박스 풀 만들기
+            foreach (var item in chatBoxItems)
+            {
+                item.gameObject.SetActive(false);
+                chatItems.Enqueue(item);
+            }
+
+            _chatRef = _firebaseManager.Database
+                .GetReference("globalChat")
+                .OrderByChild("timestamp")
+                .LimitToLast(MaxChatCount);
+
+            _chatRef.ChildAdded += OnNewMessage;
+        }
+
+        private void OnDisable()
+        {
+            _chatRef.ChildAdded -= OnNewMessage;
+        }
 
 
-    //    public void OnClickSendButton()
-    //    {
-    //        string message = _messageInputField.text.Trim();
-    //        if (string.IsNullOrEmpty(message)) return;
+        public void OnClickSendButton()
+        {
+            if (String.IsNullOrEmpty(_textInputField.text))
+            {
+                Debug.LogWarning("메시지가 비어있습니다");
+                return;
+            }
+            else if (_textInputField.text.Length > 200)
+            {
+                Debug.LogWarning("메시지가 너무 깁니다");
+                return;
+            }
 
-    //        string sender = PhotonNetwork.NickName;
-    //        object content = new object[] { sender, message };
+            string message = _textInputField.text;
+            string sender = _firebaseManager.Auth.CurrentUser.DisplayName;
 
-    //        RaiseEventOptions raiseOptions = new RaiseEventOptions
-    //        {
-    //            Receivers = ReceiverGroup.All,
-    //        };
+            ChatMessage newMessage = new ChatMessage(sender, message);
 
-    //        SendOptions sendOptions = new SendOptions
-    //        {
-    //            Reliability = true,
-    //        };
+            _firebaseManager.Database
+                .GetReference("globalChat")
+                .Push()
+                .SetRawJsonValueAsync(JsonUtility.ToJson(newMessage))
+                .ContinueWithOnMainThread(task =>
+                {
+                    if (task.IsCompletedSuccessfully)
+                    {
+                        Debug.Log("메시지 전송 성공");
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"메시지 전송 실패: {task.Exception}");
+                    }
+                });
 
-    //        PhotonNetwork.RaiseEvent(PhotonEventCodes.GlobalChatMessage, content, raiseOptions, sendOptions);
-
-    //        _messageInputField.text = string.Empty;
-    //    }
+            _textInputField.text = String.Empty;
+        }
 
 
-    //    public void OnEvent(EventData photonEvent)
-    //    {
-    //        if (photonEvent.Code == PhotonEventCodes.GlobalChatMessage)
-    //        {
-    //            object[] data = (object[])photonEvent.CustomData;
-    //            string sender = (string)data[0];
-    //            string message = (string)data[1];
+        private void OnNewMessage(object sender, ChildChangedEventArgs args)
+        {
+            if (args.Snapshot == null || !args.Snapshot.Exists) return;
 
-    //            GameObject chatBoxObj = Instantiate(_chatBoxPrefab, _chatContentParent);
-    //            ChatBoxItem chatBoxItem = chatBoxObj.GetComponent<ChatBoxItem>();
-    //            chatBoxItem.Init(sender, message);
-    //        }
-    //    }
-    //}
+            ChatMessage msg = JsonUtility.FromJson<ChatMessage>(args.Snapshot.GetRawJsonValue());
+            AddMessageToUI(msg.sender, msg.text);
+        }
+
+        private void AddMessageToUI(string sender, string message)
+        {
+            // 초과된 메시지는 재활용
+            if (activeItems.Count >= MaxChatCount)
+            {
+                ChatBoxItem oldItem = activeItems[0];
+                activeItems.RemoveAt(0);
+                oldItem.gameObject.SetActive(false);
+                chatItems.Enqueue(oldItem);
+            }
+
+            if (chatItems.Count == 0)
+            {
+                Debug.LogWarning("ChatBoxItem 큐에 여유가 없습니다.");
+                return;
+            }
+
+            ChatBoxItem item = chatItems.Dequeue();
+            item.Init(sender, message);
+            item.transform.SetAsLastSibling(); // 맨 아래로
+            item.gameObject.SetActive(true);
+
+            activeItems.Add(item);
+
+            // 스크롤 맨 아래로 내리기
+            Canvas.ForceUpdateCanvases();
+            _scrollRect.verticalNormalizedPosition = 0f;
+        }
+    }
 }
