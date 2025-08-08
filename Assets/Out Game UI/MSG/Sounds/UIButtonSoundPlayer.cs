01011eee085c27f1497e09a553b9cbca20a7ca08
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-08 13:07:55 +0900

    refactor: 오디오 싱글톤으로 변경

--- Assets/Out Game UI/MSG/Sounds/UIButtonSoundPlayer.cs
+++ Assets/Out Game UI/MSG/Sounds/UIButtonSoundPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using SHG;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -9,20 +10,18 @@ namespace MIN
 {
     public class UIButtonSoundPlayer : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
     {
-        [Inject] IAudioLibrary _audioLibrary;
-
         [SerializeField] private AudioClip _hoverSound;
         [SerializeField] private AudioClip _clickSound;
 
 
         public void OnPointerClick(PointerEventData eventData)
         {
-            _audioLibrary.PlaySfx(_clickSound);
+            SingletonAudio.Instance.PlaySfx(_clickSound);
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            _audioLibrary.PlaySfx(_hoverSound);
+            SingletonAudio.Instance.PlaySfx(_hoverSound);
         }
     }
 }
