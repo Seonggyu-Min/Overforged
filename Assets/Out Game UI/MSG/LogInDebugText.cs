994fcfeaccec3cbaaa72fbe300070cc557036700
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-08 11:13:40 +0900

    feat: 디버그용 텍스트 추가

--- Assets/Out Game UI/MSG/LogInDebugText.cs
+++ Assets/Out Game UI/MSG/LogInDebugText.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -11,16 +12,24 @@ namespace MIN
     {
         [Inject] private IFirebaseManager _firebaseManager;
         [SerializeField] private TMP_Text _debugText;
+        [SerializeField] private TMP_Text _debugText2;
 
         private void Update()
         {
+            _debugText.text = $"Player Count: {PhotonNetwork.PlayerList.Length}\n" +
+                  $"Is Master Client: {PhotonNetwork.IsMasterClient}\n" +
+                  $"Is In Room: {PhotonNetwork.InRoom}\n" +
+                  $"Is In Lobby : {PhotonNetwork.InLobby}\n" +
+                  $"Is Connected: {PhotonNetwork.IsConnected}\n" +
+                  $"PhotonNetwork.NetworkClientState : {PhotonNetwork.NetworkClientState.ToString()}";
+
             if (_firebaseManager.Auth.CurrentUser != null)
             {
-                _debugText.text = $"로그인됨: {_firebaseManager.Auth.CurrentUser.Email}";
+                _debugText2.text = $"로그인됨: {_firebaseManager.Auth.CurrentUser.Email}";
             }
             else
             {
-                _debugText.text = "로그인되지 않음";
+                _debugText2.text = "로그인되지 않음";
             }
         }
     }
